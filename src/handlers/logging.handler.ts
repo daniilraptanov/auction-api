40dbcfedf8d8d4736be48c0785ff2b40a060cbe2
Daniil Raptanov <daniilraptanov@gmail.com>
2024-02-11 21:17:12 +0200

    update of logger

--- src/handlers/logging.handler.ts
+++ src/handlers/logging.handler.ts
@@ -7,14 +7,14 @@ export const logger = (target: any, propertyKey: string, descriptor: PropertyDes
 
     descriptor.value = async function (req: Request, res: Response, next: NextFunction) {
         try {
-            console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
+            console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
 
             const result = await originalMethod.call(this, req, res, next);
 
-            console.log(`[${new Date().toISOString()}] ${req.method} ${req.path} ${res.statusCode}`);
+            console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} ${res.statusCode}`);
             return result;
         } catch (error) {
-            console.error(`[${new Date().toISOString()}] ${req.method} ${req.path} ${res.statusCode} - ${error.message}`);
+            console.error(`[${new Date().toISOString()}] ${req.method} ${req.url} ${res.statusCode} - ${error.message}`);
             return sendResponse(res, StatusCodes.INTERNAL_SERVER_ERROR);
         }
     };
